7a8dc7ecbc398dcae5d4d208508ab9f6247b8fea
AngelRosadoWTF <146990290+AngelRosadoWTF@users.noreply.github.com>
2023-12-19 11:42:15 -0600

    Update CENTRO DE MASCOTAS By Angel Rosado and Joshua Arturo.cs

--- CENTRO DE MASCOTAS By Angel Rosado and Joshua Arturo.cs
+++ CENTRO DE MASCOTAS By Angel Rosado and Joshua Arturo.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using static Program;
 
 class Program
 {
@@ -238,169 +237,38 @@ class Program
         }
     }
 
-    static void AdministracionDeMascotas()
-    {
-        static void AdministracionDeMascotas()
-        {
-            bool salir = false;
-            while (!salir)
-            {
-                Console.WriteLine("1 - Mostrar todas las mascotas registradas");
-                Console.WriteLine("2 - Registrar mascota nueva");
-                Console.WriteLine("3 - Buscar mascotas por especie");
-                Console.WriteLine("4 - Buscar mascotas por nombre");
-                Console.WriteLine("5 - Examinar mascota");
-                Console.WriteLine("6 - Volver al menú anterior");
-                Console.Write("Seleccione una opción: ");
-                int opcion = Convert.ToInt32(Console.ReadLine());
-
-                switch (opcion)
-                {
-                    case 1:
-                        MostrarTodasLasMascotas();
-                        break;
-                    case 2:
-                        RegistrarMascotaNueva();
-                        break;
-                    case 3:
-                        BuscarMascotasPorEspecie();
-                        break;
-                    case 4:
-                        BuscarMascotasPorNombre();
-                        break;
-                    case 5:
-                        ExaminarMascota();
-                        break;
-                    case 6:
-                        salir = true;
-                        break;
-                    default:
-                        Console.WriteLine("Opción no válida.");
-                        break;
-                }
-            }
-        }
-
-static void MostrarTodasLasMascotas()
-{
-    foreach (var persona in personas)
-    {
-        foreach (var mascota in persona.Mascotas)
-        {
-            Console.WriteLine($"ID: {mascota.Id}, Nombre: {mascota.Nombre}, Especie: {mascota.Especie}");
-        }
-    }
-}
-
-
-static void RegistrarMascotaNueva()
-{
-    Console.Write("Ingrese el nombre de la mascota: ");
-    string nombre = Console.ReadLine();
-
-    Console.Write("Ingrese la especie de la mascota: ");
-    string especie = Console.ReadLine();
-
-    Console.Write("Ingrese el temperamento de la mascota: ");
-    string temperamento = Console.ReadLine();
-
-    Console.Write("Ingrese la edad de la mascota: ");
-    int edad = int.Parse(Console.ReadLine());
-
-    Mascota nuevaMascota = new Mascota(personas.SelectMany(p => p.Mascotas).Count() + 1, nombre, especie, temperamento, edad);
-
-    Console.Write("¿Desea asignarle un dueño a la mascota? (s/n): ");
-    if (Console.ReadLine().ToLower() == "s")
-    {
-        AsignarDueñoAMascota(nuevaMascota);
-    }
-    else
-    {
-        // La mascota no tiene dueño
-        nuevaMascota.Dueño = null;
-    }
-
-    // Añadir la mascota a la colección global o a una lista de mascotas sin dueño
-}
-
-static void AsignarDueñoAMascota(Mascota mascota)
-{
-    // Implementar la lógica para asignar un dueño a la mascota
-}
-
-
-static void BuscarMascotasPorEspecie()
-{
-    Console.Write("Ingrese la especie de la mascota: ");
-    string especie = Console.ReadLine();
-
-    var mascotasEncontradas = personas.SelectMany(p => p.Mascotas)
-                                      .Where(m => m.Especie.Equals(especie, StringComparison.OrdinalIgnoreCase))
-                                      .ToList();
-
-    foreach (var mascota in mascotasEncontradas)
-    {
-        Console.WriteLine($"ID: {mascota.Id}, Nombre: {mascota.Nombre}, Especie: {mascota.Especie}");
-    }
-}
-
-
-static void BuscarMascotasPorNombre()
-{
-    Console.Write("Ingrese el nombre de la mascota: ");
-    string nombre = Console.ReadLine();
-
-    var mascotasEncontradas = personas.SelectMany(p => p.Mascotas)
-                                      .Where(m => m.Nombre.Contains(nombre, StringComparison.OrdinalIgnoreCase))
-                                      .ToList();
-
-    foreach (var mascota in mascotasEncontradas)
-    {
-        Console.WriteLine($"ID: {mascota.Id}, Nombre: {mascota.Nombre}, Especie: {mascota.Especie}");
-    }
-}
-
-
-static void ExaminarMascota()
-{
-    Console.Write("Ingrese el ID de la mascota: ");
-    int id = int.Parse(Console.ReadLine());
-
-    var mascota = personas.SelectMany(p => p.Mascotas).FirstOrDefault(m => m.Id == id);
-
-    if (mascota == null)
-    {
-        Console.Write("Mascota no encontrada. ¿Desea buscar por nombre? (s/n): ");
-        if (Console.ReadLine().ToLower() == "s")
-        {
-            BuscarMascotasPorNombre();
-        }
-        return;
-    }
-
-    Console.WriteLine($"ID: {mascota.Id}, Nombre: {mascota.Nombre}, Especie: {mascota.Especie}, Temperamento: {mascota.Temperamento}, Edad: {mascota.Edad}, Dueño: {mascota.Dueño?.Nombre ?? "Sin dueño"}");
-}
-
-    static void AdministracionDeAdopciones()
+    public static void AdministracionDeMascotas()
     {
         bool salir = false;
         while (!salir)
         {
-            Console.WriteLine("1 - Ver mascotas disponibles para adoptar");
-            Console.WriteLine("2 - Adoptar mascota");
-            Console.WriteLine("3 - Regresar al menú anterior");
+            Console.WriteLine("1 - Mostrar todas las mascotas registradas");
+            Console.WriteLine("2 - Registrar mascota nueva");
+            Console.WriteLine("3 - Buscar mascotas por especie");
+            Console.WriteLine("4 - Buscar mascotas por nombre");
+            Console.WriteLine("5 - Examinar mascota");
+            Console.WriteLine("6 - Volver al menú anterior");
             Console.Write("Seleccione una opción: ");
             int opcion = Convert.ToInt32(Console.ReadLine());
 
             switch (opcion)
             {
                 case 1:
-                    VerMascotasDisponibles();
+                    MostrarTodasLasMascotas();
                     break;
                 case 2:
-                    AdoptarMascota();
+                    RegistrarMascotaNueva();
                     break;
                 case 3:
+                    BuscarMascotasPorEspecie();
+                    break;
+                case 4:
+                    BuscarMascotasPorNombre();
+                    break;
+                case 5:
+                    ExaminarMascota();
+                    break;
+                case 6:
                     salir = true;
                     break;
                 default:
@@ -410,100 +278,121 @@ static void ExaminarMascota()
         }
     }
 
-    static void VerMascotasDisponibles()
-    {
-        var mascotasDisponibles = personas.SelectMany(p => p.Mascotas.Where(m => m.DisponibleParaAdopcion)).ToList();
-        foreach (var mascota in mascotasDisponibles)
+        static void MostrarTodasLasMascotas()
         {
-            Console.WriteLine($"ID: {mascota.Id}, Nombre: {mascota.Nombre}, Especie: {mascota.Especie}");
+            foreach (var persona in personas)
+            {
+                foreach (var mascota in persona.Mascotas)
+                {
+                    Console.WriteLine($"ID: {mascota.Id}, Nombre: {mascota.Nombre}, Especie: {mascota.Especie}");
+                }
+            }
         }
-    }
 
-    static void AdoptarMascota()
-    {
-        Console.Write("Ingrese el ID de la mascota que desea adoptar: ");
-        int idMascota = Convert.ToInt32(Console.ReadLine());
 
-        var mascota = personas.SelectMany(p => p.Mascotas).FirstOrDefault(m => m.Id == idMascota && m.DisponibleParaAdopcion);
-
-        if (mascota == null)
+        static void RegistrarMascotaNueva()
         {
-            Console.Write("Mascota no encontrada. ¿Desea buscar por especie? (s/n): ");
+            Console.Write("Ingrese el nombre de la mascota: ");
+            string nombre = Console.ReadLine();
+
+            Console.Write("Ingrese la especie de la mascota: ");
+            string especie = Console.ReadLine();
+
+            Console.Write("Ingrese el temperamento de la mascota: ");
+            string temperamento = Console.ReadLine();
+
+            Console.Write("Ingrese la edad de la mascota: ");
+            int edad = int.Parse(Console.ReadLine());
+
+            Mascota nuevaMascota = new Mascota(personas.SelectMany(p => p.Mascotas).Count() + 1, nombre, especie, temperamento, edad);
+
+            Console.Write("¿Desea asignarle un dueño a la mascota? (s/n): ");
             if (Console.ReadLine().ToLower() == "s")
             {
-                BuscarMascotaPorEspecie();
+                AsignarDueñoAMascota(nuevaMascota);
             }
-            return;
+            else
+            {
+                nuevaMascota.Dueño = null;
+            }
         }
 
-        RealizarAdopcion(mascota);
-    }
-
-    static void BuscarMascotaPorEspecie()
-    {
-        Console.Write("Ingrese la especie de la mascota que desea adoptar: ");
-        string especie = Console.ReadLine();
-
-        var mascotasPorEspecie = personas.SelectMany(p => p.Mascotas)
-                                          .Where(m => m.Especie.Equals(especie, StringComparison.OrdinalIgnoreCase) && m.DisponibleParaAdopcion)
-                                          .ToList();
-
-        if (mascotasPorEspecie.Count == 0)
+        static void AsignarDueñoAMascota(Mascota mascota)
         {
-            Console.WriteLine("No hay mascotas disponibles de esa especie.");
-            return;
+
         }
 
-        foreach (var mascota in mascotasPorEspecie)
+
+        static void BuscarMascotasPorEspecie()
         {
-            Console.WriteLine($"ID: {mascota.Id}, Nombre: {mascota.Nombre}, Especie: {mascota.Especie}");
+            Console.Write("Ingrese la especie de la mascota: ");
+            string especie = Console.ReadLine();
+
+            var mascotasEncontradas = personas.SelectMany(p => p.Mascotas)
+                                              .Where(m => m.Especie.Equals(especie, StringComparison.OrdinalIgnoreCase))
+                                              .ToList();
+
+            foreach (var mascota in mascotasEncontradas)
+            {
+                Console.WriteLine($"ID: {mascota.Id}, Nombre: {mascota.Nombre}, Especie: {mascota.Especie}");
+            }
         }
 
-        Console.Write("¿Desea adoptar alguna de estas mascotas? (s/n): ");
-        if (Console.ReadLine().ToLower() != "s")
+
+        static void BuscarMascotasPorNombre()
         {
-            return;
+            Console.Write("Ingrese el nombre de la mascota: ");
+            string nombre = Console.ReadLine();
+
+            var mascotasEncontradas = personas.SelectMany(p => p.Mascotas)
+                                              .Where(m => m.Nombre.Contains(nombre, StringComparison.OrdinalIgnoreCase))
+                                              .ToList();
+
+            foreach (var mascota in mascotasEncontradas)
+            {
+                Console.WriteLine($"ID: {mascota.Id}, Nombre: {mascota.Nombre}, Especie: {mascota.Especie}");
+            }
         }
 
-        Console.Write("Ingrese el ID de la mascota que desea adoptar: ");
-        int idMascota = Convert.ToInt32(Console.ReadLine());
-        var mascotaSeleccionada = mascotasPorEspecie.FirstOrDefault(m => m.Id == idMascota);
 
-        if (mascotaSeleccionada != null)
+        static void ExaminarMascota()
         {
-            RealizarAdopcion(mascotaSeleccionada);
+            Console.Write("Ingrese el ID de la mascota: ");
+            int id = int.Parse(Console.ReadLine());
+
+            var mascota = personas.SelectMany(p => p.Mascotas).FirstOrDefault(m => m.Id == id);
+
+            if (mascota == null)
+            {
+                Console.Write("Mascota no encontrada. ¿Desea buscar por nombre? (s/n): ");
+                if (Console.ReadLine().ToLower() == "s")
+                {
+                    BuscarMascotasPorNombre();
+                }
+                return;
+            }
+
+            Console.WriteLine($"ID: {mascota.Id}, Nombre: {mascota.Nombre}, Especie: {mascota.Especie}, Temperamento: {mascota.Temperamento}, Edad: {mascota.Edad}, Dueño: {mascota.Dueño?.Nombre ?? "Sin dueño"}");
         }
-        else
-        {
-            Console.WriteLine("ID no válido.");
-        }
-    }
 
-    static void RealizarAdopcion(Mascota mascota)
-    {
-
-    }
-
-    static void AdministracionDeBienestarAnimal()
-    {
-        static void AdministracionDeBienestarAnimal()
+        static void AdministracionDeAdopciones()
         {
             bool salir = false;
             while (!salir)
             {
-                Console.WriteLine("1 - Servicio de Spa");
-                Console.WriteLine("2 - Corte de pelo");
-                Console.WriteLine("3 - Volver al menú anterior");
+                Console.WriteLine("1 - Ver mascotas disponibles para adoptar");
+                Console.WriteLine("2 - Adoptar mascota");
+                Console.WriteLine("3 - Regresar al menú anterior");
                 Console.Write("Seleccione una opción: ");
                 int opcion = Convert.ToInt32(Console.ReadLine());
 
                 switch (opcion)
                 {
                     case 1:
-                        ServicioDeSpa();
+                        VerMascotasDisponibles();
                         break;
                     case 2:
-                        CorteDePelo();
+                        AdoptarMascota();
                         break;
                     case 3:
                         salir = true;
@@ -515,62 +404,134 @@ static void ExaminarMascota()
             }
         }
 
-        static void ServicioDeSpa()
+        static void VerMascotasDisponibles()
         {
-            var persona = ObtenerPersona();
-            if (persona == null) return;
-
-            MostrarMascotasYSeleccionarServicio(persona, "Spa");
+            var mascotasDisponibles = personas.SelectMany(p => p.Mascotas.Where(m => m.DisponibleParaAdopcion)).ToList();
+            foreach (var mascota in mascotasDisponibles)
+            {
+                Console.WriteLine($"ID: {mascota.Id}, Nombre: {mascota.Nombre}, Especie: {mascota.Especie}");
+            }
         }
 
-        static void CorteDePelo()
+        static void AdoptarMascota()
         {
-            var persona = ObtenerPersona();
-            if (persona == null) return;
+            Console.Write("Ingrese el ID de la mascota que desea adoptar: ");
+            int idMascota = Convert.ToInt32(Console.ReadLine());
 
-            MostrarMascotasYSeleccionarServicio(persona, "Corte de pelo");
+            var mascota = personas.SelectMany(p => p.Mascotas).FirstOrDefault(m => m.Id == idMascota && m.DisponibleParaAdopcion);
+
+            if (mascota == null)
+            {
+                Console.Write("Mascota no encontrada. ¿Desea buscar por especie? (s/n): ");
+                if (Console.ReadLine().ToLower() == "s")
+                {
+                    BuscarMascotaPorEspecie();
+                }
+                return;
+            }
+
+            RealizarAdopcion(mascota);
         }
 
-        static void MostrarMascotasYSeleccionarServicio(Persona persona, string servicio)
+        static void BuscarMascotaPorEspecie()
+        {
+            Console.Write("Ingrese la especie de la mascota que desea adoptar: ");
+            string especie = Console.ReadLine();
+
+            var mascotasPorEspecie = personas.SelectMany(p => p.Mascotas).Where(m => m.Especie.Equals(especie, StringComparison.OrdinalIgnoreCase) && m.DisponibleParaAdopcion).ToList();
+
+            if (mascotasPorEspecie.Count == 0)
+            {
+                Console.WriteLine("No hay mascotas disponibles de esa especie.");
+                return;
+            }
+
+            foreach (var mascota in mascotasPorEspecie)
+            {
+                Console.WriteLine($"ID: {mascota.Id}, Nombre: {mascota.Nombre}, Especie: {mascota.Especie}");
+            }
+
+            Console.Write("¿Desea adoptar alguna de estas mascotas? (s/n): ");
+            if (Console.ReadLine().ToLower() != "s")
+            {
+                return;
+            }
+
+            Console.Write("Ingrese el ID de la mascota que desea adoptar: ");
+            int idMascota = Convert.ToInt32(Console.ReadLine());
+            var mascotaSeleccionada = mascotasPorEspecie.FirstOrDefault(m => m.Id == idMascota);
+
+            if (mascotaSeleccionada != null)
+            {
+                RealizarAdopcion(mascotaSeleccionada);
+            }
+            else
+            {
+                Console.WriteLine("ID no válido.");
+            }
+        }
+
+        static void RealizarAdopcion(Mascota mascota)
+        {
+
+        }
+
+        static void AdministracionDeBienestarAnimal()
+        {
+            static void AdministracionDeBienestarAnimal()
+            {
+                bool salir = false;
+                while (!salir)
+                {
+                    Console.WriteLine("1 - Servicio de Spa");
+                    Console.WriteLine("2 - Corte de pelo");
+                    Console.WriteLine("3 - Volver al menú anterior");
+                    Console.Write("Seleccione una opción: ");
+                    int opcion = Convert.ToInt32(Console.ReadLine());
+
+                    switch (opcion)
+                    {
+                        case 1:
+                            ServicioDeSpa();
+                            break;
+                        case 2:
+                            CorteDePelo();
+                            break;
+                        case 3:
+                            salir = true;
+                            break;
+                        default:
+                            Console.WriteLine("Opción no válida.");
+                            break;
+                    }
+                }
+            }
+
+            static void ServicioDeSpa()
+            {
+                var persona = ObtenerPersona();
+                if (persona == null) return;
+
+                MostrarMascotasYSeleccionarServicio(persona, "Spa");
+            }
+
+            static void CorteDePelo()
+            {
+                var persona = ObtenerPersona();
+                if (persona == null) return;
+
+                MostrarMascotasYSeleccionarServicio(persona, "Corte de pelo");
+            }
+
+            static void MostrarMascotasYSeleccionarServicio(Persona persona, string servicio)
+            {
+
+            }
+        }
+
+        private static Persona ObtenerPersona()
         {
 
         }
     }
-
-    private static Persona ObtenerPersona()
-    {
-
-    }
-
-
 }
-
-
-    class AdministracionMascotas
-    {
-
-        public void MascotasRegistradas()
-        {
-
-        }
-
-        public void RegistrarMascota()
-        {
-
-        }
-
-        public void BuscarMascotaId()
-        {
-
-        }
-        public void BuscarMascotaNombre()
-        {
-
-        }
-        public void ExaminarMascota()
-        {
-
-        }
-    }
-}
-﻿
